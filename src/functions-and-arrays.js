0bcc92bdb20318a237f3ca74c5c36339deb0a543
SiyaM108 <deprominent@gmail.com>
2025-08-10 19:50:39 +0100

    complete greatestProductOfDiagonals(matrix) function

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -174,7 +174,7 @@ function howManyTimes(wordsCount, wordExample) {
   return count;
 }
 
-// Iteration #8: Bonus
+// Iteration #8: Bonus - Product of adjacent numbers
 const matrix = [
   [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
   [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
@@ -223,6 +223,15 @@ function greatestProduct(matrix) {
     }
   }
 
+  return maxProduct;
+}
+
+// Bonus - Iteration #8.1  - Product of adjacent numbers
+function greatestProductOfDiagonals(matrix) {
+  let maxProduct = 0;
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+
   // Check diagonal products (top-left to bottom-right)
   for (let i = 0; i < rows - 3; i++) {
     for (let j = 0; j < cols - 3; j++) {
@@ -245,7 +254,6 @@ function greatestProduct(matrix) {
 
   return maxProduct;
 }
-
 
 
 
