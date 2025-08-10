18fdabf4bc5d006b46bd106623797322ae423539
SiyaM108 <deprominent@gmail.com>
2025-08-10 18:29:41 +0100

    completed findLongestWord function

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -14,7 +14,15 @@ function maxOfTwoNumbers(num1, num2) {
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(words[]) {
+  let longestWord = '';
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i];
+    }
+  }
+  return longestWord;
+}
 
 
 
